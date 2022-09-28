f30eb1e7725ea6a3102858bc0f27bb2f969b562b
Marcelo Hilario <marcelolacerda2002@gmail.com>
2022-09-28 12:46:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,35 +7,35 @@ while continuar_usando == "SIM":
   print("* para Multiplicação")
   print("/ para Divisão")
 
-  operacao = input("\nQual operação você deseja realizar? ")
+  operacao = input("Qual operação você deseja realizar? ")
 
   if operacao == "+":
-    a1 = float(input("\nDigite o primeiro valor: "))
+    a1 = float(input("Digite o primeiro valor: "))
     a2 = float(input("Digite o segundo valor: "))
     adicao = a1 + a2
     print("A soma entre",a1,"e",a2,"é:",adicao)
     continuar_usando = input("Gostaria de fazer outra operação? ").upper()
 
   if operacao == "-":
-    b1 = float(input("\nDigite o primeiro valor: "))
+    b1 = float(input("Digite o primeiro valor: "))
     b2 = float(input("Digite o segundo valor: "))
     subtracao = b1 - b2
     print("A subtração entre",b1,"e",b2,"é:",subtracao)
     continuar_usando = input("Gostaria de fazer outra operação? ").upper()
 
   if operacao == "*":
-    c1 = float(input("\nDigite o primeiro valor: "))
+    c1 = float(input("Digite o primeiro valor: "))
     c2 = float(input("Digite o segundo valor: "))
     multiplicacao = c1 * c2
     print("A multiplicação entre",c1,"e",c2,"é:",multiplicacao)
     continuar_usando = input("Gostaria de fazer outra operação? ").upper()
 
   if operacao == "/":
-    d1 = float(input("\nDigite o primeiro valor: "))
+    d1 = float(input("Digite o primeiro valor: "))
     d2 = float(input("Digite o segundo valor: "))
     while d2 == 0:
       print("O segundo valor não pode ser zero!")
-      d2 = float(input("\nDigite o segundo valor (diferente de zero): "))
+      d2 = float(input("Digite o segundo valor (diferente de zero): "))
     divisao = d1 / d2
     print("A divisão entre",d1,"e",d2,"é:",divisao)
     continuar_usando = input("Gostaria de fazer outra operação? ").upper()
